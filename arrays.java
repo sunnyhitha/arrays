d557175a4758f26d7b6db82d3ab742fcb79a82f6
sunnyhitha <41725679+sunnyhitha@users.noreply.github.com>
2018-07-31 11:56:46 +0530

    Create arrays.java

---
+++ arrays.java
@@ -0,0 +1,31 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int []a=new int[n];
+int max=0;
+for(int i=0;i<n;i++)
+{
+	a[i]=sc.nextInt();
+}
+max=a[0];
+for(int i=1;i<n;i++)
+{
+	if(max<a[i])
+	{
+		max=a[i];
+	}
+}
+System.out.println(max);
+}// your code goes here
+	}
